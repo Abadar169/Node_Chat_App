32b5dcc6e1decedc2d09a38af356c25d751b8c8d
Your Name <you@example.com>
2023-06-26 07:30:12 +0530

    Changes to index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-// 12f5638b-dddc-4a61-b785-21f3231a41fe
-//532d3531-dd97-4138-a7b9-947b3d97241f
+
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
 
